a3643989468ffc788ef2623a04147a09eef00590
Markus Hubig <mhubig@gmail.com>
2013-01-30 16:04:44 +0100

    Bumped version number to release-0.8.4.
    Signed-off-by: Markus Hubig <mhubig@gmail.com>

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ CLASSIFIERS = filter(None, map(str.strip,
 
 setup(
         name = 'IMPLib2',
-        version = 'release-0.8.2',
+        version = 'release-0.8.4',
         packages = find_packages(exclude=["tests"]),
 
         # Include the *.yaml files
